63386a1986703fc9fe6410ff107dbf25d9587570
ustal <quit.ipad2@gmail.com>
2017-05-15 13:27:03 +0300

    add Test:RapidAPIBundle and PackageBundle

---
+++ src/RapidAPIBundle/Service/Manager.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rapidapi
+ * Date: 14.04.17
+ * Time: 14:28
+ */
+
+namespace RapidAPIBundle\Service;
+
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use RapidAPIBundle\Exception\PackageException;
+
+class Manager
+{
+    /** @var DataValidator */
+    private $dataValidator;
+
+    /** @var Sender */
+    private $sender;
+
+    /** @var Metadata */
+    private $metadata;
+
+    /** @var Request */
+    private $request;
+
+    /** @var array */
+    private $currentBlockMetadata = [];
+
+    public function __construct(DataValidator $dataValidator, Sender $sender, Metadata $metadata, RequestStack $requestStack)
+    {
+        $this->dataValidator = $dataValidator;
+        $this->sender = $sender;
+        $this->request = $requestStack->getCurrentRequest();
+        $this->metadata = $metadata;
+    }
+
+    public function setBlockName($blockName)
+    {
+        $this->currentBlockMetadata = $this->metadata->getBlockData($blockName);
+        $this->dataValidator->setData($this->request, $this->currentBlockMetadata);
+    }
+
+    public function getValidData()
+    {
+        return $this->dataValidator->getValidData();
+    }
+
+    public function send($url, $data, $headers = []) {
+        $blockMetadata = $this->dataValidator->getBlockMetadata();
+        if (!isset($blockMetadata['type'])) {
+            $type = 'json';
+        }
+        else {
+            $type = $blockMetadata['type'];
+        }
+
+        return $this->sender->send($url, $blockMetadata['method'], $data, $headers, $type);
+    }
+
+
+    public function createFullUrl($url = '', &$data)
+    {
+        $url = $url . $this->currentBlockMetadata['url'];
+        // like "https://{domain}.host.com" . "/getTicket/{ticket_id}"
+        // todo fix: change snake_case to camelCase
+        $res = preg_replace_callback(
+            '/{(\w+)}/',
+            function ($match) use (&$data) {
+                if (!isset($data[$match[1]])) {
+                    throw new PackageException('Cant find variables to URL parse: ' . $match[1]);
+                }
+                $result = $data[$match[1]];
+                unset($data[$match[1]]);
+                if (is_array($result)) {
+                    return str_replace(' ', '', implode(',', $result));
+                }
+                return $result;
+            },
+            $url);
+        return $res;
+    }
+
+    public function createHeaders(&$data)
+    {
+        $result = [
+            'Authorization' => 'Bearer ' . $data['access_token']
+        ];
+        unset($data['access_token']);
+
+        return $result;
+    }
+}
