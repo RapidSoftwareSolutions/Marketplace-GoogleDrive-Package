2322a5376ee24f284ca446f7608cedb57102f735
Denis Raygorodskiy <raygorodskij@rapidapi.com>
2017-09-14 13:13:47 +0300

    new version of package

---
+++ routes/createFileFromMultipartData.php
@@ -0,0 +1,110 @@
+<?php
+
+$app->post('/api/GoogleDrive/createFileFromMultipartData', function ($request, $response) {
+
+    $settings = $this->settings;
+    $checkRequest = $this->validation;
+    $validateRes = $checkRequest->validate($request, ['accessToken','uploadFile','contentType']);
+
+    if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
+        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
+    } else {
+        $post_data = $validateRes;
+    }
+
+    $requiredParams = ['accessToken'=>'access_token','uploadFile'=>'uploadFile','contentType'=>'contentType'];
+    $optionalParams = ['ignoreDefaultVisibility'=>'ignoreDefaultVisibility','keepRevisionForever'=>'keepRevisionForever','ocrLanguage'=>'ocrLanguage','supportsTeamDrives'=>'supportsTeamDrives','useContentAsIndexableText'=>'useContentAsIndexableText','appProperties'=>'appProperties','contentHintsIndexableText'=>'contentHintsIndexableText','contentHintsThumbnailImage'=>'contentHintsThumbnailImage','contentHintsMimeType'=>'contentHintsMimeType','description'=>'description','folderColorRgb'=>'folderColorRgb','id'=>'id','mimeType'=>'mimeType','modifiedTime'=>'modifiedTime','name'=>'name','originalFilename'=>'originalFilename','parents'=>'parents','properties'=>'properties','starred'=>'starred','viewedByMeTime'=>'viewedByMeTime','viewersCanCopyContent'=>'viewersCanCopyContent','writersCanShare'=>'writersCanShare'];
+    $bodyParams = [
+       'query' => ['access_token','uploadType','ignoreDefaultVisibility','ocrLanguage','supportsTeamDrives','keepRevisionForever','useContentAsIndexableText','fields','uploadFile'],
+       'json' => ['appProperties','contentHintsIndexableText','contentHintsThumbnailImage','contentHintsThumbnailMimeType','createdTime','description','folderColorRgb','id','mimeType','modifiedTime','name','originalFilename','parents','properties','starred','viewedByMeTime','viewersCanCopyContent','writersCanShare']
+    ];
+
+    $data = \Models\Params::createParams($requiredParams, $optionalParams, $post_data['args']);
+
+    
+
+    $client = $this->httpClient;
+    $query_str = "https://www.googleapis.com/upload/drive/v3/files";
+
+    
+
+    $requestParams = \Models\Params::createRequestBody($data, $bodyParams);
+    $requestParams['headers'] = ["Authorization"=>"Bearer {$data['access_token']}"];
+    $requestParams['query']['uploadType'] = 'multipart';
+
+    $requestParams['multipart'] = [
+        array('name' => 'json','contents' => json_encode($requestParams['json']),'headers' => ['Content-Type' => 'application/json']),
+        array('name' => 'file','contents' => fopen($data['uploadFile'],'r'),'headers' => ['Content-Type' => $data['contentType']])
+    ];
+
+
+    unset($requestParams['json']);
+
+    if(!empty($data['contentHintsIndexableText']))
+    {
+        $requestParams['json']['contentHints']['IndexableText'] = $data['contentHintsIndexableText'];
+    }
+
+    if(!empty($data['contentHintsThumbnailImage']))
+    {
+        $requestParams['json']['contentHints']['thumbnail']['image'] = $data['contentHintsThumbnailImage'];
+    }
+
+    if(!empty($data['contentHintsMimeType']))
+    {
+        $requestParams['json']['contentHints']['thumbnail']['mimeType'] = $data['contentHintsMimeType'];
+    }
+
+
+    try {
+        $resp = $client->post($query_str, $requestParams);
+        $responseBody = $resp->getBody()->getContents();
+
+        if(in_array($resp->getStatusCode(), ['200', '201', '202', '203', '204'])) {
+            $result['callback'] = 'success';
+            $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+            if(empty($result['contextWrites']['to'])) {
+                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+            }
+        } else {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = json_decode($responseBody);
+        }
+
+    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody()->getContents();
+        if(empty(json_decode($responseBody))) {
+            $out = $responseBody;
+        } else {
+            $out = json_decode($responseBody);
+        }
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $out;
+
+    } catch (GuzzleHttp\Exception\ServerException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody()->getContents();
+        if(empty(json_decode($responseBody))) {
+            $out = $responseBody;
+        } else {
+            $out = json_decode($responseBody);
+        }
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $out;
+
+    } catch (GuzzleHttp\Exception\ConnectException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'INTERNAL_PACKAGE_ERROR';
+        $result['contextWrites']['to']['status_msg'] = 'Something went wrong inside the package.';
+
+    }
+
+    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
+
+});
---
+++ routes/subscribeToFileChanges.php
@@ -0,0 +1,89 @@
+<?php
+
+$app->post('/api/GoogleDrive/subscribeToFileChanges', function ($request, $response) {
+
+    $settings = $this->settings;
+    $checkRequest = $this->validation;
+    $validateRes = $checkRequest->validate($request, ['accessToken','fileId']);
+
+    if(!empty($validateRes) && isset($validateRes['callback']) && $validateRes['callback']=='error') {
+        return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($validateRes);
+    } else {
+        $post_data = $validateRes;
+    }
+
+    $requiredParams = ['accessToken'=>'access_token','fileId'=>'fileId'];
+    $optionalParams = ['acknowledgeAbuse'=>'acknowledgeAbuse','supportsTeamDrives'=>'supportsTeamDrives','kind'=>'kind','id'=>'id','resourceId'=>'resourceId','resourceUri'=>'resourceUri','token'=>'token','expiration'=>'expiration','type'=>'type','address'=>'address','payload'=>'payload','params'=>'params','fields'=>'fields'];
+    $bodyParams = [
+       'query' => ['access_token','acknowledgeAbuse','supportsTeamDrives'],
+       'json' => ['kind','id','resourceId','resourceUri','token','expiration','type','address','payload','params']
+    ];
+
+    $data = \Models\Params::createParams($requiredParams, $optionalParams, $post_data['args']);
+
+
+    if(!empty($data['fields']))
+    {
+        $data['fields'] = \Models\Params::toString($data['fields'], ',');
+    }
+    $client = $this->httpClient;
+    $query_str = "https://www.googleapis.com/drive/v3/files/{$data['fileId']}/watch";
+
+    
+
+    $requestParams = \Models\Params::createRequestBody($data, $bodyParams);
+    $requestParams['headers'] = [];
+     
+
+    try {
+        $resp = $client->post($query_str, $requestParams);
+        $responseBody = $resp->getBody()->getContents();
+
+        if(in_array($resp->getStatusCode(), ['200', '201', '202', '203', '204'])) {
+            $result['callback'] = 'success';
+            $result['contextWrites']['to'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+            if(empty($result['contextWrites']['to'])) {
+                $result['contextWrites']['to']['status_msg'] = "Api return no results";
+            }
+        } else {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = json_decode($responseBody);
+        }
+
+    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody()->getContents();
+        if(empty(json_decode($responseBody))) {
+            $out = $responseBody;
+        } else {
+            $out = json_decode($responseBody);
+        }
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $out;
+
+    } catch (GuzzleHttp\Exception\ServerException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody()->getContents();
+        if(empty(json_decode($responseBody))) {
+            $out = $responseBody;
+        } else {
+            $out = json_decode($responseBody);
+        }
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $out;
+
+    } catch (GuzzleHttp\Exception\ConnectException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'INTERNAL_PACKAGE_ERROR';
+        $result['contextWrites']['to']['status_msg'] = 'Something went wrong inside the package.';
+
+    }
+
+    return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
+
+});
